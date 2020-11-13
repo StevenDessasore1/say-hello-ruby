08d4523c7feb23ee3d5e3e6185e2d67e03b3f53e
StevenDessasore <stevendessasoreba@gmail.com>
2020-11-13 10:36:27 -0500

    Done.

--- spec/say_hello_spec.rb
+++ spec/say_hello_spec.rb
@@ -10,6 +10,5 @@ describe "say_hello" do
   it 'defaults to Ruby Programmer when no name is passed in' do
     expect($stdout).to receive(:puts).with("Hello Ruby Programmer!")
     say_hello()
-  end
 end
-
+end
